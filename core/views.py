8db1f811282f666ff1b1f00a63edce916146be36
Faezeh <shahmirzaei.faezeh.ou@gmail.com>
2023-03-02 16:56:02 +0330

    change urls and htmls product details

--- core/views.py
+++ core/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.views import View
+from product.models import Product
 
 
 # Create your views here.
 class HomeView(View):
-    def get (self, request):
-        return render(request, 'home.html')
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, 'home.html', {'product': products})
+# return render(request, 'home.html')
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, "detail.html", {'product': product})
--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -11,11 +12,11 @@ class Brand(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100,null=True)
+    slug = models.SlugField(max_length=100, null=True)
 
     class Meta:
         ordering = ('name',)
-        verbose_name='category'
+        verbose_name = 'category'
         verbose_name_plural = 'categories'
 
     def __str__(self):
@@ -41,25 +42,29 @@ class Discount(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100, unique=True,null=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True)
     brand = models.ForeignKey(Brand, on_delete=models.DO_NOTHING)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     price = models.IntegerField(default=0)
-    product_image = models.ImageField(upload_to='./media/product_images/')
+    product_image = models.ImageField(upload_to='product/%Y%m')
     unavailable = 1
     available = 2
     STATUS_CHOICES = ((unavailable, 'ناموجود'), (available, 'موجود'))
     status = models.IntegerField(choices=STATUS_CHOICES, blank=True)
     description = models.TextField()
     discount = models.ManyToManyField(Discount, blank=True)
-    create_time=models.DateTimeField(auto_now_add=True)
-    update_time=models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering=('name',)
+        ordering = ('name',)
 
     def __str__(self):
         return f'{self.name}, {self.price}'
+
+    def get_absolute_url(self):
+        return reverse('home:product_detail', args=[self.slug,])
+
     #
     # @price
     # def price(self):
